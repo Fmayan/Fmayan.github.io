36f9077bb38d2ab3d6896a1eed0211af499000be
Fmayan <kalmochi@gmail.com>
2019-12-17 04:44:43 -0500

    js file

--- Moody Music Final/js/index.js
+++ Moody Music Final/js/index.js
@@ -1,1 +1,56 @@
 
+function bigImg1(x) {
+  x.style.height = "336px";
+  x.style.width = "264px";
+}
+function normalImg1(x) {
+  x.style.height = "285px";
+  x.style.width = "221px";
+}
+
+function bigImg2(x) {
+  x.style.height = "352px";
+  x.style.width = "287px";
+}
+function normalImg2(x) {
+  x.style.height = "276px";
+  x.style.width = "221px";
+}
+
+
+function bigImg3(x) {
+  x.style.height = "320px";
+  x.style.width = "290px";
+}
+function normalImg3(x) {
+  x.style.height = "286px";
+  x.style.width = "258px";
+}
+
+function bigImg4(x) {
+  x.style.height = "346px";
+  x.style.width = "277px";
+}
+function normalImg4(x) {
+  x.style.height = "285px";
+  x.style.width = "221px";
+}
+
+function bigImg5(x) {
+  x.style.height = "1054px";
+  x.style.width = "1054px";
+}
+function normalImg5(x) {
+  x.style.height = "1024px";
+  x.style.width = "1024px";
+}
+
+
+function pickMood(){
+	var button = document.getElementById("happy")
+	if (button.value == '0') {
+		document.body.style.backgroundColor = "00000";
+		button.value = '1';
+	}
+}
+
